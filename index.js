d550804a3f13870c6aa7d86d84afedcfa5b6596c
Chris Masters <chrismasters@gmail.com>
2018-12-07 16:55:10 +0100

    Saves screenshot in user directory with id filename

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const puppeteer = require('puppeteer');
 const argv = require('minimist')(process.argv.slice(2));
+const url = require('url');
+const fs = require('fs');
+const chalk = require('chalk');
 
 (async () => {
-  const url = argv._[0] || 'https://twitter.com/joshburt76/status/506760039424868352';
+  const tweet = argv._[0];
+
+  if (!tweet) {
+    const errorMsg = chalk.red('Please provide a tweet URL to screenshot.');
+    console.log(errorMsg);
+    
+    process.exit();
+  }
+
+  const tweetURL = url.parse(tweet);
+  
+  const [,username,,tweetId] = tweetURL.pathname.split("/");
+
+  let path = './tweets';
+
+  if (!fs.existsSync(path)){
+    fs.mkdirSync(path);
+  }
+
+  path += `/${username}`;
+
+  if (!fs.existsSync(path)){
+    fs.mkdirSync(path);
+  }
+
+  path += `/${tweetId}.png`
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -11,20 +39,23 @@ const argv = require('minimist')(process.argv.slice(2));
     height: 600,
     deviceScaleFactor: 2
   })
-  await page.goto(url);
+  await page.goto(tweet);
 
   await page.evaluate(() => {
     document.querySelector('div.tweet').style.borderRadius = 0;
     document.querySelector('div.follow-bar').style.display = 'none';
   });
 
-  const tweet = await page.$('div.tweet');
-  const tweetDimensions = await tweet.boundingBox();  
+  const tweetEl = await page.$('div.tweet');
+  const tweetDimensions = await tweetEl.boundingBox();  
 
   await page.screenshot({
-    path: 'example.png',
+    path,
     clip: tweetDimensions
   });
+
+  const successMsg = chalk.green(`Screenshot successfully saved to ${path}`);
+  console.log(successMsg);
 
   await browser.close();
 })();
